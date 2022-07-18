56c109e3590cb5dc7ecbd36bda01ff22a22b179d
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-07-18 22:23:28 +0200

    Add socket and create connection event

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const socket = require("socket.io");
 
 const app = express();
 
@@ -12,6 +13,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
 
 app.use("/api", testimonialRoutes);
 app.use("/api", concertsRoutes);
@@ -25,6 +31,12 @@ app.use((req, res) => {
   res.status(404).json({ message: "Not found..." });
 });
 
-app.listen(process.env.PORT || 8000, () => {
+const server = app.listen(process.env.PORT || 8000, () => {
   console.log("Server is running on port: 8000");
+});
+
+const io = socket(server);
+
+io.on("connection", (socket) => {
+  console.log("New socket " + socket.id);
 });
