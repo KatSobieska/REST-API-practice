e34acb45db02c0c44a2b29155653fe5b995ef020
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-07-25 21:37:00 +0200

    Add mongoose connection to db

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const socket = require("socket.io");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -13,17 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, "/client/build")));
-
-const server = app.listen(process.env.PORT || 8000, () => {
-  console.log("Server is running on port: 8000");
-});
-
-const io = socket(server);
-
-app.use((req, res, next) => {
-  req.io = io;
-  next();
-});
 
 app.use("/api", testimonialRoutes);
 app.use("/api", concertsRoutes);
@@ -37,6 +26,19 @@ app.use((req, res) => {
   res.status(404).json({ message: "Not found..." });
 });
 
-io.on("connection", (socket) => {
-  console.log("New socket " + socket.id);
+mongoose.connect("mongodb://localhost:27017/NewWaveDB", {
+  useNewUrlParser: true,
+});
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+  console.log("Connected to the database");
+});
+db.on("error", (err) => {
+  console.log("Error" + err);
+});
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log("Server is running on port: 8000");
 });
