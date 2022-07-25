f6bac970bc532e97c3076c6cc652c9ee2b673ead
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-07-25 22:25:12 +0200

    Modify endpoints and move them into controllers folder

---
+++ controllers/seats.controller.js
@@ -0,0 +1,71 @@
+const Seat = require("../models/seat.model");
+
+exports.getAll = async (req, res) => {
+  try {
+    res.json(await Seat.find());
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+exports.getById = async (req, res) => {
+  try {
+    const currentSeat = await Seat.findById(req.params.id);
+    if (!currentSeat) res.status(404).json({ message: "Not found" });
+    else res.json(currentSeat);
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+exports.addSeat = async (req, res) => {
+  try {
+    const { day, seat, client, email } = req.body;
+    const newSeat = new Seat({
+      day: day,
+      seat: seat,
+      client: client,
+      email: email,
+    });
+    await newSeat.save();
+    res.json({ message: "OK" });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+exports.updateSeat = async (req, res) => {
+  const { day, seat, client, email } = req.body;
+
+  try {
+    const currentSeat = await Seat.findById(req.params.id);
+    if (currentSeat) {
+      await Seat.updateOne(
+        { _id: req.params.id },
+        {
+          $set: {
+            day: day,
+            seat: seat,
+            client: client,
+            email: email,
+          },
+        }
+      );
+      res.json({ message: "OK" });
+    } else res.status(404).json({ message: "Not found..." });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+exports.deleteSeat = async (req, res) => {
+  try {
+    const currentSeat = await Seat.findById(req.params.id);
+    if (currentSeat) {
+      await Seat.deleteOne({ _id: req.params.id });
+      res.json({ message: "OK" });
+    } else res.status(404).json({ message: "Not found..." });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
---
+++ controllers/testimonials.controller.js
@@ -0,0 +1,67 @@
+const Testimonial = require("../models/testimonial.model");
+
+exports.getAll = async (req, res) => {
+  try {
+    res.json(await Testimonial.find());
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+exports.getById = async (req, res) => {
+  try {
+    const tsm = await Testimonial.findById(req.params.id);
+    if (!tsm) res.status(404).json({ message: "Not found" });
+    else res.json(tsm);
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+exports.addTestimonial = async (req, res) => {
+  try {
+    const { author, text } = req.body;
+    const newTestimonial = new Testimonial({
+      author: author,
+      text: text,
+    });
+    await newTestimonial.save();
+    res.json({ message: "OK" });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+exports.updateTestimonial = async (req, res) => {
+  const { author, text } = req.body;
+
+  try {
+    const tsm = await Testimonial.findById(req.params.id);
+    if (tsm) {
+      await Testimonial.updateOne(
+        { _id: req.params.id },
+        {
+          $set: {
+            author: author,
+            text: text,
+          },
+        }
+      );
+      res.json({ message: "OK" });
+    } else res.status(404).json({ message: "Not found..." });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+exports.deleteTestimonial = async (req, res) => {
+  try {
+    const tsm = await Testimonial.findById(req.params.id);
+    if (tsm) {
+      await Testimonial.deleteOne({ _id: req.params.id });
+      res.json({ message: "OK" });
+    } else res.status(404).json({ message: "Not found..." });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
